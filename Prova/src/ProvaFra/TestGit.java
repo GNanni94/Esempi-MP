0ed5bba78500a44d11af08c5490bb4b9d39e0093
GNanni94 <g.nanni94@hotmail.it>
2025-03-14 15:30:50 +0100

    aggiornamento stato

--- Prova/src/ProvaFra/TestGit.java
+++ Prova/src/ProvaFra/TestGit.java
@@ -1,8 +1,8 @@
 package ProvaFra;
 
 public class TestGit {
-	private void Syso() {
-		// TODO Auto-generated method stub
-
+	
+	public static void main (String[] args) {
+		System.out.println("Dio santooo");
 	}
 }
